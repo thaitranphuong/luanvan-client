13b6f2690f2f66558f253984391c5a74f5a8bb18
Thai Tran <tranphuongthai000@gmail.com>
2024-02-05 20:38:44 +0700

    update notification

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -2,6 +2,7 @@ import Icon from '@mdi/react';
 import styles from './NavLeft.module.scss';
 import { mdiAccountOutline, mdiBellOutline, mdiTextBoxOutline, mdiTicketPercentOutline } from '@mdi/js';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 function NavLeft() {
     return (
@@ -11,24 +12,44 @@ function NavLeft() {
                 <Icon style={{ color: 'blue', marginRight: '10px' }} path={mdiAccountOutline} size={1.5} />
                 Tài Khoản Của Tôi
             </Link>
-            <Link to="/user/account" className={styles.line_item}>
+            <Link
+                to="/user/account"
+                className={clsx(styles.line_item, { [styles.active]: window.location.pathname.includes('account') })}
+            >
                 Hồ Sơ
             </Link>
-            <Link to="/user/address" className={styles.line_item}>
+            <Link
+                to="/user/address"
+                className={clsx(styles.line_item, { [styles.active]: window.location.pathname.includes('address') })}
+            >
                 Địa Chỉ
             </Link>
-            <Link to="/user/change-password" className={styles.line_item}>
+            <Link
+                to="/user/change-password"
+                className={clsx(styles.line_item, {
+                    [styles.active]: window.location.pathname.includes('change-password'),
+                })}
+            >
                 Đổi Mật Khẩu
             </Link>
-            <Link to="/user/purchase" className={styles.line}>
+            <Link
+                to="/user/purchase"
+                className={clsx(styles.line, { [styles.active]: window.location.pathname.includes('purchase') })}
+            >
                 <Icon style={{ color: 'blue', marginRight: '10px' }} path={mdiTextBoxOutline} size={1.5} />
                 Đơn Mua
             </Link>
-            <Link to="/user/notification" className={styles.line}>
+            <Link
+                to="/user/notification"
+                className={clsx(styles.line, { [styles.active]: window.location.pathname.includes('notification') })}
+            >
                 <Icon style={{ color: '#f7482e', marginRight: '10px' }} path={mdiBellOutline} size={1.5} />
                 Thông Báo
             </Link>
-            <Link to="/user/voucher" className={styles.line}>
+            <Link
+                to="/user/voucher"
+                className={clsx(styles.line, { [styles.active]: window.location.pathname.includes('voucher') })}
+            >
                 <Icon style={{ color: '#f7482e', marginRight: '10px' }} path={mdiTicketPercentOutline} size={1.5} />
                 Kho Voucher
             </Link>
--- src/pages/user/Notification/index.js
+++ src/pages/user/Notification/index.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Footer from '../../../Layout/DefaultLayout/Footer';
 import NavLeft from '../../../components/NavLeft';
 import styles from './Notification.module.scss';
@@ -7,7 +8,38 @@ function Notification() {
         <>
             <div className={styles.wrapper}>
                 <NavLeft />
-                <div className={styles.box}></div>
+                <div className={styles.box}>
+                    <div className={clsx(styles.item, { [styles.not_yet_read]: true })}>
+                        <img className={styles.img} src={require('../../../assets/images/product.png')} alt="" />
+                        <div className={styles.info}>
+                            <div className={styles.title}>Giao kiện hàng thành công</div>
+                            <div className={styles.content}>
+                                Kiện hàng <strong>1221321</strong> đã giao thành công đến bạn
+                            </div>
+                            <div className={styles.date}>10:54 25-01-2024</div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.item, { [styles.not_yet_read]: false })}>
+                        <img className={styles.img} src={require('../../../assets/images/product.png')} alt="" />
+                        <div className={styles.info}>
+                            <div className={styles.title}>Giao kiện hàng thành công</div>
+                            <div className={styles.content}>
+                                Kiện hàng <strong>1221321</strong> đã giao thành công đến bạn
+                            </div>
+                            <div className={styles.date}>10:54 25-01-2024</div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.item, { [styles.not_yet_read]: false })}>
+                        <img className={styles.img} src={require('../../../assets/images/product.png')} alt="" />
+                        <div className={styles.info}>
+                            <div className={styles.title}>Giao kiện hàng thành công</div>
+                            <div className={styles.content}>
+                                Kiện hàng <strong>1221321</strong> đã giao thành công đến bạn
+                            </div>
+                            <div className={styles.date}>10:54 25-01-2024</div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
